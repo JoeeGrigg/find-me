bb7a826e8282fbec94caed8ff20256f566e09c88
Joe Grigg <joe@joegrigg.com>
2026-05-01 07:07:41 +0100

    Show random OpenStreetMap location

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
 
 get '/' do
+  @latitude = rand(-85.0..85.0).round(6)
+  @longitude = rand(-180.0..180.0).round(6)
+
+  map_delta = 0.05
+  @map_bbox = [
+    @longitude - map_delta,
+    @latitude - map_delta,
+    @longitude + map_delta,
+    @latitude + map_delta
+  ].map { |coordinate| coordinate.round(6) }.join(',')
+
   erb :index
 end
